0e3402e761c2290e88a718738577b8e277e141e3
Ion <19718355+Nei-V@users.noreply.github.com>
2019-04-23 01:19:15 +0300

    prodcuts controller finished

--- server/controllers/products_controller.js
+++ server/controllers/products_controller.js
@@ -1,3 +1,24 @@
+const Product = require('../models/product') //has to be ouside of module.exports to be global variable
+
 module.exports = {
+    readAllProducts(req, res) {
+        Product.find({}).exec((err, products) => { //empty object to return all products
+            if (err) console.log('Get all products Mongoose error --------', err);
+            //When using mongoose can use a callback or a use a exec method to catch and respond to errors.
+            console.log('products----------',products);
+            //After the data is received send a status code of 200 and the data via the send method.
+            res.status(200).send(products);
+            //this can also be used as response: res.status(200).json({products})
+        });
+    },
+    readProduct(req, res) {
+        //Destruct the id from req.params - we get it from the endpoint
+        const { id } = req.params;
+        Product.findById({id}).exec((err, product) => {
+            if (err) console.log('Get single product error -------', err);
+            console.log('product--------', product);
+            res.status(200).json({product});
+        });
+    }
 
 };
