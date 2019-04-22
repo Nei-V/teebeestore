b948f2142b0a53363ec70ac4118b49a1e4e935af
Ion <19718355+Nei-V@users.noreply.github.com>
2019-04-23 00:29:30 +0300

    finnishing index.js server file-creating endpoints

--- server/index.js
+++ server/index.js
@@ -12,23 +12,31 @@ const productsController = require('./controllers/products_controller');
 
 const MongoClient = require('mongodb').MongoClient;
 
+//import express server
 const express = require('express');
 
+//instance of the server called "app" where all endpoint will go through
 const app = express();
 
+//port -> has to be the same as proxy
 const PORT = 3000;
 
+//MongoDB client
 const uri = process.env.CONNECTION_STRING;
 const client = new MongoClient(uri, {useNewUrlParser: true });
 client.connect(err => {
     const collection = client.db("test").collection('devices');
     // perform actions on the collection object
-    console.log("connected?");
+    console.log("connectetion error");
     client.close();
 });
 
+//Middleware
+//For initializing the req.body. If the middleware is not used, the req.body is undefined.
+
 app.use(bodyParser.json());
 
+//session -> for cookies
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false, //this for resaving the cookie false, if true can cause a memory leak.
@@ -38,18 +46,33 @@ app.use(session({
     }
 }));
 
+//cors -> allow cross origin requests
 app.use(cors());
 
+//setTimeout -> because of race condition, in order not to have errors (is asynchronous)
 setTimeout(() => {
     //all our endpoints
 }, 200);
 
+
+//USER endpoints:
 app.get('/api/user-data', userController.readUserData);
-
 app.post('/api/user-data/cart/:id', userController.addToCart);
-
 app.delete('api/user-date/cart/:id', userController.removeFromCart);
+//because it uses Auth0 - which means users log with social media account, there is no need to register - to login is enough
+app.post('/api/login', userController.login);
+app.post('/api/logout', userController.logout);
 
-app.post('/api/login', userController.login)
+//PRODUCTS endpoints - this is for showing (reading) products from database (reading is enough beacuse only the admin can create, update or delete products)
+app.get('api/products', productsController.readAllProducts); //getting all products
+app.get('api/products/:id',productsController.readProduct); //getting specific product using request paramter
 
-app.post('/api/logout', userController.logout)
+//ADMIN endpoints:
+app.get('api/users',adminController.getAdminUsers);
+app.post('api/products',adminController.createProduct); //admin creates new product in the database, that's why no "id" parameter is needed
+app.put('api/products/:id', adminController.updateProduct); // needs "id" to access specific product in the database
+app.delete('api/products/:id',adminController.deleteProduct); //needs "id" to delete specific product from the database
+
+
+//listen to our app on the port defined before:
+app.listen(PORT,() => console.log('Listening on port',PORT));
